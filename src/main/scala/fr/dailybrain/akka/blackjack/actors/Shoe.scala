409d5d01264bf48167fc165a84599fdb92550cfb
Vincent Spiewak <vspiewak@gmail.com>
2018-06-05 19:51:20 +0200

    Smalls refactoring on Shoe actor

--- src/main/scala/fr/dailybrain/akka/blackjack/actors/Shoe.scala
+++ src/main/scala/fr/dailybrain/akka/blackjack/actors/Shoe.scala
@@ -3,20 +3,22 @@ import akka.actor.{Actor, Props}
 
 import fr.dailybrain.akka.blackjack.actors.Messages._
 import fr.dailybrain.akka.blackjack.models.Deck.new52
-import fr.dailybrain.akka.blackjack.models.{Card, CutCard, PlayingCard}
+import fr.dailybrain.akka.blackjack.models.{Card, CutCard}
 
 import scala.util.Random.shuffle
 
 object Shoe {
-  def props(numberOfDecks: Int, penetration: Int): Props = Props(new Shoe(numberOfDecks, penetration))
+
+  def props(numberOfDecks: Int, cutCardPosition: Int): Props =
+    Props(new Shoe(numberOfDecks, cutCardPosition))
+
 }
 
-class Shoe(numberOfDecks: Int, penetration: Int) extends Actor {
+class Shoe(numberOfDecks: Int, cutCardPosition: Int) extends Actor {
 
-  override def preStart() =
-    self ! Shuffle
+  override def preStart: Unit = self ! Shuffle
 
-  def receive = active(Seq.empty)
+  def receive: Receive = active(Seq.empty)
 
   def active(cards: Seq[Card]): Receive = {
 
@@ -27,8 +29,8 @@ class Shoe(numberOfDecks: Int, penetration: Int) extends Actor {
       context become active(rest)
 
     case Shuffle =>
-      val decks: Seq[PlayingCard] = shuffle((1 to numberOfDecks).flatMap(_ => new52()))
-      val (front, back) = decks.splitAt(penetration)
+      val decks = shuffle((1 to numberOfDecks).flatMap(_ => new52()))
+      val (front, back) = decks.splitAt(cutCardPosition)
       val newShoe = front ++ Seq(CutCard) ++ back
       context become active(newShoe)
 
