d06995a89d426c84ee0ec56b69d6ec69ea95f5f2
Vincent Spiewak <vspiewak@gmail.com>
2018-08-12 17:03:46 +0200

    Simplify for yield in Deck.gen52

--- src/main/scala/fr/dailybrain/akka/blackjack/models/Deck.scala
+++ src/main/scala/fr/dailybrain/akka/blackjack/models/Deck.scala
@@ -6,15 +6,14 @@ object Deck {
 
   def gen52(): Seq[PlayingCard] = {
 
-    val suits = Seq(♠, ♥, ♣, ♦)
-    //
-    val ranks = Seq(`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`) ++
-                Seq(Jack, Queen, King, Ace)
-
     for {
-      s <- suits
-      r <- ranks
-    } yield PlayingCard(r, s)
+
+      suit <- Seq(♠, ♥, ♣, ♦)
+
+      rank <- Seq(`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`) ++
+           Seq(Jack, Queen, King, Ace)
+
+    } yield PlayingCard(rank, suit)
 
   }
 
